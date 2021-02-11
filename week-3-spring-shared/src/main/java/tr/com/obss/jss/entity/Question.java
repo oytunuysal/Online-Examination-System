441c4ef06d029ce594858b0a6a7f4369308aebf9
sevgiaydn <sevgiaydin1999@hotmail.com>
2021-02-11 21:16:25 +0300

    question

---
+++ week-3-spring-shared/src/main/java/tr/com/obss/jss/entity/Question.java
@@ -0,0 +1,75 @@
+package tr.com.obss.jss.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "QUESTION")
+public class Question extends EntityBase{
+
+    @Column(name = "TEXT", length = 255)
+    private String questionText;
+
+    @Column(name = "POINT")
+    private int point;
+
+ //   @OneToMany(mappedBy = "QUESTION")
+  //  private List<String> possibleAnswers;
+
+    @Column(name = "ANSWER")
+    private String answer;
+
+    @Column(name = "PENALTY_POINT")
+    private double penaltyPoint;
+
+    @ManyToOne
+    private Exam exam;
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+/*    public List<String> getPossibleAnswers() {
+        return possibleAnswers;
+    }
+
+    public void setPossibleAnswers(List<String> possibleAnswers) {
+        this.possibleAnswers = possibleAnswers;
+    }*/
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public double getPenaltyPoint() {
+        return penaltyPoint;
+    }
+
+    public void setPenaltyPoint(double penaltyPoint) {
+        this.penaltyPoint = penaltyPoint;
+    }
+
+    public Exam getExam() {
+        return exam;
+    }
+
+    public void setExam(Exam exam) {
+        this.exam = exam;
+    }
+}
