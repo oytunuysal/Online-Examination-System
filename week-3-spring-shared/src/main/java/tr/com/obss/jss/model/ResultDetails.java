2fe595174a06eb9434f93fd365883333beb11250
yaren <yarenbergi25@gmail.com>
2021-02-12 15:36:06 +0300

    Result service ve controller

---
+++ week-3-spring-shared/src/main/java/tr/com/obss/jss/model/ResultDetails.java
@@ -0,0 +1,6 @@
+package tr.com.obss.jss.model;
+
+import java.io.Serializable;
+
+public interface ResultDetails extends Serializable {
+}
--- week-3-spring-shared/src/main/java/tr/com/obss/jss/repo/ResultRepository.java
+++ week-3-spring-shared/src/main/java/tr/com/obss/jss/repo/ResultRepository.java
@@ -1,11 +1,19 @@
 package tr.com.obss.jss.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import tr.com.obss.jss.entity.Exam;
 import tr.com.obss.jss.entity.Result;
 import tr.com.obss.jss.entity.Role;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Long> {
+    @Query(value = "select * from Exam e where e.id = :id", nativeQuery = true)
+    Optional<Result> getByIdNative(long id);
 
+    List<Result> findByNameStartingWithAndOperationTypeIsNotNullAndActiveTrueOrderByIdDesc(String name);
 }
